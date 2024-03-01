69561c1af8482ff6b6147e4bd7f3a135a30bb91b
Onelots <54822802+Oneloutre@users.noreply.github.com>
2024-03-01 09:03:29 +0100

    :ambulance: fixed critical bug

--- connection.py
+++ connection.py
@@ -1,5 +1,4 @@
 from discord.ext import commands, tasks
-from discord.ui import Button
 import discord
 import apis.pont_chaban as pont_chaban
 import datetime
@@ -13,18 +12,9 @@ bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
 )
 
 async def pont(ctx):
-    nextBoat = pont_chaban.getAllBoat()
-    embed = discord.Embed(title="Lien de l'api utilisée", url="https://opendata.bordeaux-metropole.fr/explore/dataset/previsions_pont_chaban/api/", colour=0x00b0f4,
-                          description=f"Voilà les prochaines dates auxquelles le pont chaban sera inaccessible.\n\n**{nextBoat[0][1]} | __{nextBoat[0][0]}__:**\n__Fermeture__ : {nextBoat[0][2]}\n__Ouverture__ :{nextBoat[0][3]}\n ---------------------------- \n"
-                                      f"\n**{nextBoat[1][1]} | __{nextBoat[1][0]}__ :**\n__Fermeture__ : {nextBoat[1][2]}\n__Ouverture__ :{nextBoat[1][3]}\n ---------------------------- \n"
-                                      f"\n**{nextBoat[2][1]} | __{nextBoat[2][0]}__ :**\n__Fermeture__ : {nextBoat[2][2]}\n__Ouverture__ :{nextBoat[2][3]}\n ---------------------------- \n"
-                                      f"\n**{nextBoat[3][1]} | __{nextBoat[3][0]}__ :**\n__Fermeture__ : {nextBoat[3][2]}\n__Ouverture__ :{nextBoat[3][3]}\n ---------------------------- \n"
-                                      f"\n**{nextBoat[4][1]} | __{nextBoat[4][0]}__ :**\n__Fermeture__ : {nextBoat[4][2]}\n__Ouverture__ :{nextBoat[4][3]}\n ---------------------------- \n")
-    embed.set_author(name="Levée et fermeture du pont Chaban")
-    embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Logo_Efrei_2022.svg/512px-Logo_Efrei_2022.svg.png")
-    embed.set_image(url="https://passion-aquitaine.ouest-france.fr/wp-content/uploads/2018/02/chaban-bassins-flot.jpg")
+    embed = SendPont()
     await ctx.response.send_message(embed=embed)
-    print(nextBoat)
+
 
 @bot.tree.command(
     name="aide",
@@ -39,13 +29,10 @@ async def aide(ctx):
                       timestamp=datetime.datetime.now())
     embed.set_author(name="Liste des commandes disponibles")
     embed.set_thumbnail(url="https://cdn.onelots.fr/u/4ZW8nv.jpg")
+    embed.set_image(url="https://cdn.onelots.fr/u/HiJNgf.png")
     embed.set_footer(text="With ❤️ by Mathis & Roch", icon_url="https://cdn.onelots.fr/u/EQndLM.svg")
-    button = Button(label="/pont", style=discord.ButtonStyle.blurple)
-    #button.callback = pont()
-    view = discord.ui.View()
-    view.add_item(button)
+    await ctx.response.send_message(embed=embed)
 
-    await ctx.response.send_message(embed=embed, view=view)
 
 async def openingNOW(time, ctx):
     embed = discord.Embed(title="Lien de l'api utilisée", url="https://opendata.bordeaux-metropole.fr/explore/dataset/previsions_pont_chaban/api/", colour=0x00b0f4,
@@ -60,7 +47,7 @@ async def openingNOW(time, ctx):
 async def on_ready():
     await bot.tree.sync()
     await bot.change_presence(activity=discord.Game(name="Scraper MY BOI"), status=discord.Status.online)
-    await bot.get_channel(1212154817355710474).send("Redémarré • 🔃")
+    await bot.get_channel(1213023082197946418).send("Redémarré • 🔃")
     await getNextOpening.start()
 
 
@@ -70,4 +57,18 @@ async def getNextOpening():
     if(getNextOpening[0]):
         await openingNOW(getNextOpening[1])
 
-    
+def SendPont():
+    nextBoat = pont_chaban.getAllBoat()
+    embed = discord.Embed(title="Lien de l'api utilisée", url="https://opendata.bordeaux-metropole.fr/explore/dataset/previsions_pont_chaban/api/", colour=0x00b0f4,
+                          description=f"Voilà les prochaines dates auxquelles le pont chaban sera inaccessible.\n"
+                                      f"\n**{nextBoat[0][1]} | __{nextBoat[0][0]}__:**\n__Fermeture__ : {nextBoat[0][2]}\n__Ouverture__ :{nextBoat[0][3]}\n ---------------------------- \n"
+                                      f"\n**{nextBoat[1][1]} | __{nextBoat[1][0]}__ :**\n__Fermeture__ : {nextBoat[1][2]}\n__Ouverture__ :{nextBoat[1][3]}\n ---------------------------- \n"
+                                      f"\n**{nextBoat[2][1]} | __{nextBoat[2][0]}__ :**\n__Fermeture__ : {nextBoat[2][2]}\n__Ouverture__ :{nextBoat[2][3]}\n ---------------------------- \n"
+                                      f"\n**{nextBoat[3][1]} | __{nextBoat[3][0]}__ :**\n__Fermeture__ : {nextBoat[3][2]}\n__Ouverture__ :{nextBoat[3][3]}\n ---------------------------- \n"
+                                      f"\n**{nextBoat[4][1]} | __{nextBoat[4][0]}__ :**\n__Fermeture__ : {nextBoat[4][2]}\n__Ouverture__ :{nextBoat[4][3]}\n ---------------------------- \n")
+    embed.set_author(name="Levée et fermeture du pont Chaban")
+    embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Logo_Efrei_2022.svg/512px-Logo_Efrei_2022.svg.png")
+    embed.set_image(url="https://passion-aquitaine.ouest-france.fr/wp-content/uploads/2018/02/chaban-bassins-flot.jpg")
+    return embed
+
+
