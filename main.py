3333f249795d57b740c7f7989e0a1dc51c414791
Onelots <54822802+Oneloutre@users.noreply.github.com>
2024-03-02 14:59:39 +0100

    :bug: using dotenv now since it's more convenient

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import connection
-from config import BOT_TOKEN
+import os
+from dotenv import load_dotenv
 
-connection.bot.run(BOT_TOKEN)
+dotenv = load_dotenv()
+token = os.getenv('BOT_TOKEN')
+
+
+if __name__ == "__main__":
+    if not os.path.exists(".env"):
+        print("Le fichier .env n'existe pas, veuillez le créer et y mettre le token du bot")
+        token = str(input("Veuillez entrer le token du bot : "))
+        with open(".env", "w") as file:
+            file.write(f"BOT_TOKEN={token}")
+    connection.bot.run(token)
